32c6dfcd75933485d5c220a17a06c679ba742770
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-04-03 18:26:22 +0530

    First dotfiles

---
+++ private_dot_config/i3/executable_init.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Load the other display using xrandr
+xrandr --output DP-0 --primary --right-of HDMI-0
+
+# Load wallpaper with feh
+feh --bg-center ~/wallpapers/4k-Wallpaper-Download-61-Group-Wallpapers.jpg
+
+# Load other services
+# bus-daemon -dr
+
+
