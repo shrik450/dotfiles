e21c0572e35040884acd505aae63021553a81bbc
Shrikanth Upadhayaya <shrik450@gmail.com>
2021-05-17 08:28:18 +0530

    Switch mac like bindings
    I found using the hodge-podge bindings for both the WM and apps
    with Super was a mess, so I swapped the WM's bindings to Alt and
    moved to using Super as Ctrl.

--- private_dot_config/mac_custom_bindings.py
+++ private_dot_config/mac_custom_bindings.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # autostart = true
 
-import re
 from xkeysnail.transform import *
 
 terminals = [
@@ -9,7 +8,7 @@ terminals = [
     "konsole",
     "kitty",
 ]
-terminals = [ term.casefold() for term in terminals ]
+terminals = [term.casefold() for term in terminals]
 
 define_modmap(
     {
@@ -17,31 +16,47 @@ define_modmap(
     }
 )
 
-define_keymap(
-    lambda wm_class: wm_class not in terminals,
+define_conditional_modmap(
+    lambda wm_class: wm_class.casefold() not in terminals,
     {
-        K("Super-c"): K("C-c"),
-        K("Super-x"): K("C-x"),
-        K("Super-v"): K("C-v"),
-        K("Super-k"): K("C-k"),
-        K("Super-z"): K("C-z"),
-        K("Super-Shift-z"): K("C-Shift-z"),
-        K("Super-s"): K("C-s"),
-        K("Super-o"): K("C-o"),
-        K("Super-n"): K("C-n"),
-        K("Super-b"): K("C-b"),
-        K("Super-t"): K("C-t"),
-        K("Super-w"): K("C-w"),
-        K("Super-Shift-t"): K("C-Shift-t"),
-        K("Super-r"): K("C-r"),
-        K("Super-Shift-left_brace"): K("C-page_up"),
-        K("Super-Shift-right_brace"): K("C-page_down"),
-        K("Super-p"): K("C-p"),
-        K("Super-l"): K("C-l"),
-        K("Super-Shift-p"): K("C-Shift-p"),
-        K("Super-comma"): K("C-comma"),
-        K("Super-slash"): K("C-slash"),
-        K("Super-d"): K("C-d"),
-   },
-    "Default mac-style for non-terminal use."
+        Key.LEFT_CTRL: Key.LEFT_META,
+        Key.LEFT_META: Key.LEFT_CTRL
+    }
 )
+
+define_keymap(
+    None,
+    {
+        K("C-Shift-left_brace"): K("C-page_up"),
+        K("C-Shift-right_brace"): K("C-page_down"),
+    }
+)
+
+# define_keymap(
+#     lambda wm_class: wm_class not in terminals,
+#     {
+#         K("Super-c"): K("C-c"),
+#         K("Super-x"): K("C-x"),
+#         K("Super-v"): K("C-v"),
+#         K("Super-k"): K("C-k"),
+#         K("Super-z"): K("C-z"),
+#         K("Super-Shift-z"): K("C-Shift-z"),
+#         K("Super-s"): K("C-s"),
+#         K("Super-o"): K("C-o"),
+#         K("Super-n"): K("C-n"),
+#         K("Super-b"): K("C-b"),
+#         K("Super-t"): K("C-t"),
+#         K("Super-w"): K("C-w"),
+#         K("Super-Shift-t"): K("C-Shift-t"),
+#         K("Super-r"): K("C-r"),
+#         K("Super-Shift-left_brace"): K("C-page_up"),
+#         K("Super-Shift-right_brace"): K("C-page_down"),
+#         K("Super-p"): K("C-p"),
+#         K("Super-l"): K("C-l"),
+#         K("Super-Shift-p"): K("C-Shift-p"),
+#         K("Super-comma"): K("C-comma"),
+#         K("Super-slash"): K("C-slash"),
+#         K("Super-d"): K("C-d"),
+#    },
+#     "Default mac-style for non-terminal use."
+# )
