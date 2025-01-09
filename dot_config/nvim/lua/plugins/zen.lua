8efd578c1bdc8194058424e3af9401c34166b316
Shrikanth Upadhayaya <shrik450@gmail.com>
2025-01-09 10:59:17 -0500

    Add neovim and ghostty configs
    Along with a couple more aliases

---
+++ dot_config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    plugins = {
+      options = {
+        ruler = true,
+      },
+    },
+  },
+}
---
+++ dot_config/private_fish/functions/clippy.fish
@@ -0,0 +1,4 @@
+function clippy --description 'alias clippy cargo clippy'
+  cargo clippy $argv
+        
+end
