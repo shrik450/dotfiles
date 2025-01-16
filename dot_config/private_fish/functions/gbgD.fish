8bda741eba3e5945ddd244355b12f396633fe83b
Shrikanth Upadhayaya <shrik450@gmail.com>
2025-01-16 12:13:42 -0500

    more aliases

--- dot_config/private_fish/functions/gbgD.fish
+++ dot_config/private_fish/functions/gbgD.fish
@@ -1,4 +1,4 @@
-function gbgD
+function gbgD --wraps="LANG=C git branch --no-color -vv | grep \": gone\\]\" | cut -c 3- | awk '\\''{print }'\\'' | xargs git branch -D" --description "alias gbgD LANG=C git branch --no-color -vv | grep \": gone\\]\" | cut -c 3- | awk '\\''{print }'\\'' | xargs git branch -D"
     set -lx LANG C
 
     git branch -vv | string match -r ".*: gone\].*" | string trim -l | string replace -r "^\* " "" | string replace -r ' .*$' '' | while read branch
