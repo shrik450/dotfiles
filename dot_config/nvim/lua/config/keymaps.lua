edbfc55c84d7973242694443a77070364310a0ee
Shrikanth Upadhayaya <shrik450@gmail.com>
2025-04-28 16:59:58 -0400

    neovim: remap j and k to gj and gk

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "j", "gj", { desc = "Down (wrapped)" })
+vim.keymap.set("n", "k", "gk", { desc = "Up (wrapped)" })
+
+vim.keymap.set("n", "gj", "j", { desc = "Down (unwrapped)" })
+vim.keymap.set("n", "gk", "k", { desc = "Up (unwrapped)" })
