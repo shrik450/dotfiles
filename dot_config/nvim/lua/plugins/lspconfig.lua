fa460b63b031124fc5f9f4f5a0c410e930c776ee
Shrikanth Upadhayaya <shrikanth@allspice.io>
2026-03-26 18:55:53 -0400

    Fix monorepo handling

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -2,5 +2,13 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      basedpyright = {
+        root_dir = function(fname)
+          local util = require("lspconfig.util")
+          return util.find_git_ancestor(fname) or util.root_pattern("pyproject.toml")(fname)
+        end,
+      },
+    },
   },
 }
